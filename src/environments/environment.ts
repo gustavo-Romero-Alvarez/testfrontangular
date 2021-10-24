1bddf7a006ec19910daf044467d5ad3af42c7107
gitgusgus27 <72162281+gitgusgus27@users.noreply.github.com>
2021-10-24 20:20:20 -0300

    sin token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,7 @@ export const environment = {
   production: true,
   //HOST: 'http://localhost:8080',
   HOST: 'https://testbackendspring.herokuapp.com',
-  TOKEN_AUTH_USERNAME: 'codemediapp',
-  TOKEN_AUTH_PASSWORD: 'code89codex',
-  TOKEN_NAME: 'access_token',
-  REINTENTOS: 3
+
 };
 
 
