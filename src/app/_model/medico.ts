612b5016236ccdd88ebed8c2dfdfc25077649d05
gitgusgus27 <72162281+gitgusgus27@users.noreply.github.com>
2021-10-24 20:04:48 -0300

    primer servicio

---
+++ src/app/_model/medico.ts
@@ -0,0 +1,7 @@
+export class Medico{
+    idMedico : number | undefined;
+    nombres: string | undefined;
+    apellidos: string | undefined;
+    cmp: string | undefined;
+    fotoUrl: string | undefined;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { MedicoService } from './../app/_service/medico.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'testfrontangular';
+  constructor(
+    private medicoservice: MedicoService
+    
+  ) { }
+  ngOnInit(): void {
+     console.log("sin error")
+     this.getmedico();
+  }
+
+  getmedico(){
+    this.medicoservice.listar().subscribe(data => {
+      console.log(data);   
+    });
+  }
 }
